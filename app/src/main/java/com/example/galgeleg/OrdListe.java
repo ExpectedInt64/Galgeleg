ae6cd23fec068e3f79144f94bc24b40b106274a0
Mark <s174881@student.dtu.dk>
2020-10-05 12:18:32 +0200

    Ordliste får nu information fra et google sheets Arrayliste

--- app/src/main/java/com/example/galgeleg/OrdListe.java
+++ app/src/main/java/com/example/galgeleg/OrdListe.java
@@ -1,8 +1,6 @@
 package com.example.galgeleg;
 
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -13,36 +11,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import java.util.ArrayList;
 
 
+
 public class OrdListe extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemClickListener {
-    /*Handler uiThread = new Handler(Looper.getMainLooper());
-    Runnable opgave;
-    ArrayList<String> ordAL = new ArrayList<String>();*/
+    ArrayList<String> ordAL = new ArrayList<>();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ArrayAdapter adapter;
-        OrdData ordData = new OrdData();
+        ArrayAdapter adapter = null;
+        final OrdData ordData = new OrdData();
         ordData.execute("");
         while(!ordData.getDataReady()){
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
         }
-        String[] ord = new String[ordData.getOrdListe().size()];
-        for(int i = 0; i < ordData.getOrdListe().size();i++){
-            ord[i] = ordData.getOrdListe().get(i);
-        }
-
-        adapter = new ArrayAdapter(this,R.layout.ordliste,R.id.textView3, ord);
-
+        ordAL = ordData.getOrdListe();
+        adapter = new ArrayAdapter(this, R.layout.ordliste, R.id.textView3, ordAL);
 
         ListView listView = new ListView(this);
         listView.setOnItemClickListener(this);
         listView.setAdapter(adapter);
-
         setContentView(listView);
     }
 
