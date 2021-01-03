a1bd2a6ab406ad0664cfac15c150a394a96c6bf1
Mark <s174881@student.dtu.dk>
2021-01-03 22:53:49 +0100

    Musik + animation tilføjet

--- app/src/main/java/com/example/galgeleg/LooserActivity.java
+++ app/src/main/java/com/example/galgeleg/LooserActivity.java
@@ -3,20 +3,32 @@ package com.example.galgeleg;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class LooserActivity extends AppCompatActivity {
+
+    Animation rotateAnimation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_looser);
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.defeat);
+        mp.start();
         TextView attempts = findViewById(R.id.textView7);
         TextView ord = findViewById(R.id.textView10);
+        ImageView image = findViewById(R.id.imageView5);
         attempts.setText(getIntent().getStringExtra("attempts"));
         ord.setText("Ordet var: "+getIntent().getStringExtra("word"));
+
+        rotateAnimation= AnimationUtils.loadAnimation(this,R.anim.rotate);
+        image.startAnimation(rotateAnimation);
     }
 
     @Override
--- app/src/main/java/com/example/galgeleg/WinnerActivity.java
+++ app/src/main/java/com/example/galgeleg/WinnerActivity.java
@@ -3,21 +3,36 @@ package com.example.galgeleg;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 
 public class WinnerActivity extends AppCompatActivity {
 
+    Animation rotateAnimation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_winner);
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.victory);
+        mp.start();
         TextView attempts = findViewById(R.id.textView7);
         TextView ord = findViewById(R.id.textView10);
+        ImageView image = findViewById(R.id.imageView5);
         attempts.setText(getIntent().getStringExtra("attempts"));
         ord.setText("Ordet var: "+getIntent().getStringExtra("word"));
+
+        rotateAnimation= AnimationUtils.loadAnimation(this,R.anim.rotate);
+        image.startAnimation(rotateAnimation);
+
     }
     @Override
     public void onBackPressed() {
